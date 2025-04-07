8b77e7624c16bfe099066b8feaf9e29799f8fb63
jose <jose7arevalo7moreno7@gmail.com>
2025-04-07 15:14:13 -0500

    avance registrar usuario de mi pc

--- app/src/main/java/com/idat/agenda/MainActivity.java
+++ app/src/main/java/com/idat/agenda/MainActivity.java
@@ -1,6 +1,9 @@
 package com.idat.agenda;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button Btn_Login, Btn_Registro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,23 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Btn_Login = findViewById(R.id.Btn_Login);
+        Btn_Registro = findViewById(R.id.Btn_Registro);
+
+        Btn_Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Login.class));
+            }
+        });
+
+        Btn_Registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Registro.class));
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/idat/agenda/Registro.java
@@ -0,0 +1,31 @@
+package com.idat.agenda;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Registro extends AppCompatActivity {
+
+    EditText NombreEt, CorreoEt, ContraseñaEt, ConfirmarContraseñaEt;
+    Button RegistrarUsuario;
+    TextView TengounacuentaTXT;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_registro);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
